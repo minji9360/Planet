c84028cec53ffca2938ad7e2a301b012dd5e0987
jtbc <jtbc@naver.com>
2020-12-06 21:37:08 +0900

    첫 화면 디자인 마크업 및 scss 수정, js 추가
    - 버튼 두 개만 넣기엔 허전해서 회원가입, 로그인 창이 바로 나오도록 첫 화면 변경
    - 로그인 시 바로 to do list로 이동하도록 할 것
    - 게스트로 구경 시 샘플 페이지 구경하고 간단한 기능 테스트 할 수
      있도록 권한을 줄 것(데이터 완전 추가/수정/삭제 되는 것은 아니고 보이는
      부분만 수정되도록)
    - 사진 추가할 것
    - 로그인, 회원가입 폼 나타나도록 하는 js 추가
    - js 파일에서 주석처리 된 부분은 사진 랜덤으로 선택하고 보여주는
      부분인데 이미지가 보이지 않아 우선 주석처리 해둠

---
+++ src/assets/js/home.js
@@ -0,0 +1,32 @@
+// const body = document.querySelector("body");
+// const IMG_NUMBER = 5;
+
+// const { SingleEntryPlugin } = require("webpack");
+
+// function paintImage(imgNumber){
+//     const image = new Image();
+//     image.src = `/src/img/blue${imgNumber + 1}.jpg`;
+//     image.classList.add("bgImage");
+//     body.appendChild(image);
+// }
+
+// function genRandom() {
+//     const number = Math.floor(Math.random() * IMG_NUMBER);
+//     return (number);
+// }
+const signup = document.querySelector(".sign-up__link");
+const signin = document.querySelector(".sign-in__link");
+
+function toggleForm(){
+    var container = document.querySelector(".container");
+    container.classList.toggle("active");
+}
+
+function init(){
+    // const randomNumber = genRandom();
+    // paintImage(1);
+    signup.addEventListener("click", toggleForm);
+    signin.addEventListener("click", toggleForm);
+}
+
+init();
