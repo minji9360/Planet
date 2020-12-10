38615064a2ae7a38fd705aaf0f28b70e8afe7a3e
jtbc <jtbc@naver.com>
2020-12-10 13:07:36 +0900

    plan model 생성
    할 일 관련 모델 생성

---
+++ src/models/Plan.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+const PlanSchema = new mongoose.Schema({
+    createdAt: {
+        // 생성된 날짜
+        type: Date,
+        default: Date.now
+    },
+    title: {
+        // 할 일 제목
+        type: String,
+        required: "Title is required"
+    },
+    content: {
+        // 할 일 상세 내용
+        type: String,
+        required: false
+    },
+    feedback: {
+        // 할 일 종료 후 피드백 내용
+        type: String,
+        required: false
+    },
+    completed: { 
+        // 진행중인지 완료인지 확인하는 용도
+        type: String,
+        default: false
+    },
+    category: { 
+        // 일상, 공부 나누는 용도
+        type: String,
+        required: "Category is required"
+    }
+});
+
+const model = mongoose.model("Plan", PlanSchema);
+export default model;
