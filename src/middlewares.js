13abdc5d9fb6db3cd43ab374dd9ec392b26b8ae4
jtbc <jtbc@naver.com>
2020-12-11 15:30:19 +0900

    로그인 여부에 따라 다르게 보이도록 header 수정
    - 테스트를 위해 middleware에 임시로 isAuthenticated 정보 추가

--- src/middlewares.js
+++ src/middlewares.js
@@ -3,5 +3,8 @@ import routes from "./routes.js";
 export const localsMiddleware = (req, res, next) => {
     res.locals.siteName = "Green Day";
     res.locals.routes = routes;
+    res.locals.user = {
+        isAuthenticated: true
+    }
     next();
 }
