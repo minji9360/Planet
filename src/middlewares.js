9dc9c20d23146d981b081dbb1cbbc2cb6978ad7c
jtbc <jtbc@naver.com>
2020-12-12 06:33:18 +0900

    로그인 여부에 따라 접근 가능한 페이지 설정
    - 로그인되어 있으면 회원가입/로그인 페이지에 접속할 수 없어야 해서 메인 화면이
      plans가 되어야 해서 onlyPublic 미들웨어 생성
    - 로그인이 되어 있지 않으면 프로필이나 비밀번호 변경 페이지에 접속할 수
      없도록 onlyPrivate 미들웨어 생성

--- src/middlewares.js
+++ src/middlewares.js
@@ -6,3 +6,19 @@ export const localsMiddleware = (req, res, next) => {
     res.locals.user = req.user || null;
     next();
 };
+
+export const onlyPublic = (req, res, next) => {
+    if(req.user){
+        res.redirect(routes.plans);
+    } else {
+        next();
+    }
+}
+
+export const onlyPrivate = (req, res, next) => {
+    if(req.user) {
+        next();
+    } else {
+        res.redirect(routes.home);
+    }
+}
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -10,16 +10,17 @@ import {
     editProfile, 
     users 
 } from "../controllers/userController.js";
+import { onlyPublic, onlyPrivate } from "../middlewares.js";
 import routes from "../routes.js";
 
 const userRouter = express.Router();
 
-userRouter.get(routes.home, home);
-userRouter.post(routes.login, postLogin);
-userRouter.post(routes.join, postJoin, postLogin);
-userRouter.get(routes.logout, logout);
-userRouter.get(routes.users, users);
-userRouter.get(routes.editProfile, editProfile);
-userRouter.get(routes.changePassword, changePassword);
+userRouter.get(routes.home, onlyPublic, home);
+userRouter.post(routes.login, onlyPublic, postLogin);
+userRouter.post(routes.join, onlyPublic, postJoin, postLogin);
+userRouter.get(routes.logout, onlyPrivate, logout);
+userRouter.get(routes.users, onlyPrivate, users);
+userRouter.get(routes.editProfile, onlyPrivate, editProfile);
+userRouter.get(routes.changePassword, onlyPrivate, changePassword);
 
 export default userRouter;
