cc7937c9ec05b5b6e7988c5d4580d213c5f823eb
minji kim <minjikim9360@i-bms.co.kr>
2021-08-17 23:56:34 +0900

    feat: 달력 조회 Controller 수정
    저장된 데이터를 조회할 수 있게 넘겨주도록 코드 수정
    (기존에는 데이터 전달하지 않고 페이지 퍼블리싱만 했음)

--- src/controllers/calendarController.js
+++ src/controllers/calendarController.js
@@ -1,2 +1,18 @@
-export const calendar = (req, res) =>
-	res.render("calendar", { pageTitle: "Calendar" });
+import thisWeek from "../week.js";
+
+export const calendar = async (req, res) => {
+	try {
+		const user = await User.findById(req.user._id)
+			.populate({ path: "plans", populate: { path: "feedback" } })
+			.populate("sentence");
+
+		if (user.id !== req.user.id) {
+			throw Error();
+		} else {
+			res.render("calendar", { pageTitle: "Calendar", user, thisWeek });
+		}
+	} catch (error) {
+		console.log(error);
+		res.render("calendar", { pageTitle: "Calendar", user: [], thisWeek });
+	}
+};
